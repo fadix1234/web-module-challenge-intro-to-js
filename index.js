a557a6205accc851011cb39f9e84bcac9502f2b2
fadix1234 <ahmadzaiyasmin@yahoo.com>
2022-11-11 18:27:42 -0800

    Done

--- index.js
+++ index.js
@@ -139,36 +139,35 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
             expect(received).toBe(expected) // Object.is equality
 
             Expected: 0.2
-            Received: undefined
+            Received: undefined  
+            
 
-              21 | describe('hungryDogFunction', ()=>{ 
-              22 |   it('Dog is 1 year and is 5lbs or less', ()=>{
-            > 23 |     expect(functions.hungryDog(4, 1)).toBe(0.2);
-                |
-          ^
-              24 |   })
       
         Notice the expected and received, expected is what the test is looking for, and received is what was actually returned from this function. You can also see it's passing in two values, the number 4 and the number 1. 
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(weight, age){
-if (age > = 1 && weight <= 5){
+function hungryDog(weight,age){
+  if(age >= 1 && weight <=5){
     return weight * 0.05;
-} else if (age >= 1 && weight >= 6 && weight <= 10){
+}else if(age >= 1 && weight >= 6 && weight <= 10){
   return weight * 0.04;
-}else if ( age >= 1 && weight >= 11 && weight <= 15){
+}else if(age >= 1 && weight >= 11 && weight <= 15){
   return weight * 0.03;
-} else if (age >= 1 && weight > 15 ){
+}else if(age >= 1 && weight > 15){
   return weight * 0.02;
-} else if ( age < 1 && age >= 0.583){
+}else if(age < 1 && age >= 0.583){
   return weight * 0.04;
-} else if ( age < 0.583 && age >= 0.333){
+}else if(age < 0.583 && age >= 0.333){
   return weight * 0.05;
-} else if (age < 0.333){
+}else if(age < 0.333){
   return weight * 0.10;
+}else{
+  return 'please try again';
 }
-console.log (hungryDog (15, 1));
+}
+
+console.log(hungryDog (15, 1));
 
 
 
@@ -192,6 +191,7 @@ Use the game function below to do the following:
  - tie should return "it's a tie"
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
+*/
 let computer = Math.random();
 
 if(computer <= 0.34){
@@ -231,10 +231,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
-
+console.log(miles(3));
 
 
 //Task 5b - Centimeters to Feet
@@ -245,10 +245,10 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
-
+console.log (feet(180));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -265,9 +265,12 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+      for(let i = number; i > 0; i --){
+        return `${i} bottles of soda on the wall, ${i} bottles of soda take one down pass it around ${i-1} bottles of soda on the wall`;
+      }
 }
+console.log (annoyingSong(8));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -285,9 +288,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+if(number >= 90){
+  return `you got an A`;
+} else if(number < 90 && number >= 80){
+  return `you got a B`;
+}else if(number <80 && number >= 70){
+  return `you got a C`;
+}else if(number < 70 && number >= 60){
+  return `you got a D`
+}else if(number <60){
+  return `you got an F`;
 }
+}
+console.log(grade(90));
 
 
 
